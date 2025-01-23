1783a07b77e9a5c73d63641ddcb62989434cb681
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-24 00:51:53 +0530

    UserService creation

--- src/main/java/com/example/learn/service/UserService.java
+++ src/main/java/com/example/learn/service/UserService.java
@@ -1,4 +1,15 @@
 package com.example.learn.service;
 
+import com.example.learn.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
 }
