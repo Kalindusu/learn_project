7b165e946063559341c1819e83d955af5cd39ebb
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-23 21:28:59 +0530

    update the UserController

--- src/main/java/com/example/learn/controller/UserController.java
+++ src/main/java/com/example/learn/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.learn.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping(value = "api/v1")
 public class UserController {
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "All Users";
+    }
+
 }
