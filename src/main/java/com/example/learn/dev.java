2dc6a32326fb7c26c4007913b7d298a125e5dbd5
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-10 00:46:37 +0530

    dependency injection

--- src/main/java/com/example/learn/dev.java
+++ src/main/java/com/example/learn/dev.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Component;
 public class dev {
     @Autowired //field injection
 private  Laptop laptop;
+
+    //constructor injection
+    public dev(Laptop laptop){
+        this.laptop=laptop;
+    }
+
+    //setter injection
+    @Autowired
+    public void setLaptop(Laptop laptop){
+        this.laptop=laptop;
+    }
     public void build(){
         laptop.compile();
         System.out.println("Working on the awesome project");
